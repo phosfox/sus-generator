3b9bcfb33ad627f76c21d0ca193955fa36207a00
phosfox <mordredur@googlemail.com>
2021-07-23 21:12:04 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <img alt="Amogus" src="./assets/amogus.png" :style="{width: '400px', height: auto}" />
+  <SusGenerator/>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import SusGenerator from './components/SusGenerator.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    SusGenerator
+  }
+})
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/SusGenerator.vue
@@ -0,0 +1,39 @@
+<template>
+  <h1>{{ sus }}</h1>
+  <button @click="randomSus">SUS</button>
+</template>
+
+<script lang="ts">
+import { ref, defineComponent } from 'vue'
+import {susses} from "../assets/susses.js"
+
+const sample = (arr: string[]) => {
+  return arr[Math.floor(Math.random() * arr.length)];
+};
+
+const capitalize = (s: string) => {
+  return s.charAt(0).toUpperCase() + s.slice(1);
+}
+
+export default defineComponent({
+  name: 'SusGenerator',
+  setup: () => {
+    const sus = ref("sus");
+    const randomSus = () => {
+      sus.value = capitalize(sample(susses))
+    }
+    return { sus, randomSus}
+  }
+})
+</script>
+
+<style scoped>
+button {
+  padding: 10px 14px 10px 14px;
+  align-items: baseline;
+  color: white;
+  background-color: #7b0838;
+  border-radius: 10%;
+  border-color: white;
+}
+</style>
